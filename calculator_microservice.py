3513183c82785c71a32cf28122e4c4ecb1e749ef
SFhectorj <baezahectorj@gmail.com>
2025-11-15 12:46:34 -0800

    calcuclator_microservice.py: Added file logging compatibilty

--- calculator_microservice.py
+++ calculator_microservice.py
@@ -1,7 +1,27 @@
 import os
+import time
 
-REQUEST_PREFIX = "client_"
-REQUEST_SUFFIX = "_request.txt"
-RESPONSE_SUFFIX = "_response.txt"
+REQUEST_FILE = "_request"
+RESPONSE_FILE = "_response"
 LOG_FILE = "calc_log.txt"
+
+def log(message):
+    '''
+    User Story 3: Logs User interaction
+    Satisfies Reliability requirment
+    '''
+    with open(LOG_FILE, "a") as f:
+        f.write(message + "\n")
+
+def add(a, b): return a + b
+def sub(a, b): return a - b
+def mult(a, b): return a * b
+def div(a, b):
+    if b == 0:
+        return "ERROR"
+    return a / b
+
+ops = { "+": add, "-": sub, "*": mult, "/": div}
+
+print("Calulator microservice is running...")
 
